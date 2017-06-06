0adc50ba5e024fb9525c587da56e900233b31454
九条涼果 <s1131234@gmail.com>
2017-06-06 21:11:59 +0800

    修正逻辑错误

--- 6-26/main.cpp
+++ 6-26/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <random>
 #include <memory>
+#include <iomanip>
 
 
 int main() {
@@ -11,20 +12,22 @@ int main() {
     std::cin >> row >> column;
     std::uniform_int_distribution<int> uniform_dist(1, row * column);
     data = new int[column * row]{0};
-
+    std::cout << "转置前" << std::endl;
+    const int width = static_cast<int>(std::log10(row * column)) + 2;
     for (int i = 0; i < row; i++) {
         for (int j = 0; j < column; j++) {
-            data[i * row + j] = uniform_dist(engine);
-            std::cout << data[i * row + j] << " ";
+            data[i * column + j] = uniform_dist(engine);
+            std::cout << std::setw(width) << data[i * column + j];
         }
         std::cout << std::endl;
     }
+    std::cout << "转置后" << std::endl;
     int *temp = new int[row * column]{0};
     int count = 0;
     for (int i = 0; i < column; i++) {
         for (int j = 0; j < row; j++) {
-            temp[count] = data[i + j * column];
-            std::cout << temp[count] << " ";
+            temp[count] = data[j * column + i];
+            std::cout << std::setw(width) << temp[count];
             count++;
         }
         std::cout << std::endl;
